84ff0220d836b7443473584771a1c065c39f64a8
fanxinli <fanxin_li@qq.com>
2021-08-14 15:07:19 +0800

    add wp_template.py

---
+++ wp_template.py
@@ -0,0 +1,23 @@
+from pwn import *
+
+
+ld_path = ""
+libc_path = ""
+p = process([ld_path, ""], env={"LD_PRELOAD":libc_path})
+# p = process([ld_path, ""])
+# p = process("", env={"LD_PRELOAD":libc_path})
+# p = process("")
+# p = remote("")
+
+# context.log_level = "debug"
+
+r = lambda : p.recv()
+rx = lambda x: p.recv(x)
+ru = lambda x: p.recvuntil(x)
+rud = lambda x: p.recvuntil(x, drop=True)
+s = lambda x: p.send(x)
+sl = lambda x: p.sendline(x)
+sa = lambda x, y: p.sendafter(x, y)
+sla = lambda x, y: p.sendlineafter(x, y)
+shell = lambda : p.interactive()
+
